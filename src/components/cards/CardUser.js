141d522b3a4d6fdf56ce6afb6810d8c7af3134ff
ana788 <anabolinhu@outlook.com>
2023-09-19 21:59:36 -0300

    add useState showCard

--- src/components/cards/CardUser.js
+++ src/components/cards/CardUser.js
@@ -1,41 +1,55 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import './CardUser.css'
 
-const handleDelete = async (idUser) => {
-    const objUser = {
-        id: idUser
-    }
-
-    const response = await fetch(
-        'http://localhost:3300/user',
-        {
-            cache: 'no-store',
-            method: 'DELETE',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(objUser) //tranforma um objeto em uma string json
-        }
-    )
-    console.log(await response.json())
-}
 
 const CardUser = ({ user }) => {
-    return (
-        <div className="card-user">
-            <img src={user.avatar} width="70px" height="70px" />
-            <div>
-                <h4>{user.name}</h4>
-                <p>{user.email}</p>
-                <p>
-                    <span className='delete' onClick={() => handleDelete(user.id)}>Excluir</span>
-                    <span>Editar</span>
-                </p>
+    const [showCard, setShowCard] = useState(true)
 
-            </div>
-        </div>
+    const handleDelete = async (idUser) => {
+        const objUser = {
+            id: idUser
+        }
+    
+        const response = await fetch(
+            'http://localhost:3300/user',
+            {
+                cache: 'no-store',
+                method: 'DELETE',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(objUser) //tranforma um objeto em uma string json
+            }
+        )
+        if(response.ok){
+            const result = await response.json()
+            if(result?.sucess){
+                setShowCard(false)
+            }
+        }
+        
+    }
+
+    return (
+        <>
+            {
+                showCard ?
+                <div className="card-user">
+                    <img src={user.avatar} width="70px" height="70px" />
+                    <div>
+                        <h4>{user.name}</h4>
+                        <p>{user.email}</p>
+                        <p>
+                            <span className='delete' onClick={() => handleDelete(user.id)}>Excluir</span>
+                            <span>Editar</span>
+                        </p>
+
+                    </div>
+                </div> : null 
+            }
+        </>
     )
 }
 
