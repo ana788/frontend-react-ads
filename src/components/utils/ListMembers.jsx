f198895d532564248573bff2323268e7a5019504
ana788 <anabolinhu@outlook.com>
2023-10-03 22:09:07 -0300

    edit crud, crud finalidado

--- src/components/utils/ListMembers.jsx
+++ src/components/utils/ListMembers.jsx
@@ -7,65 +7,65 @@ import Modal from "@/components/utils/Modal"
 const ListMembers = () => {
 
     const [users, setUsers] = useState([])
-    const [modalCadastrar, setmodalCadastrarIsOpen] = useState(false)
+    const [modalSignInIsOpen, setModalSignInIsOpen] = useState(false)
 
-    useEffect (() => {
+    useEffect(() => {
         const getMembers = async () => {
             const response = await fetch('http://localhost:3300/user/list', { cache: 'no-store' })
             const data = await response.json()
             setUsers(data)
         }
         getMembers()
-    }, []) 
+    }, [])
 
-const handleSubmit = async(event) => {
-    event.preventDefault()
-    const newUser = {
-        name: event.target.name.value,
-        email: event.target.email.value,
-        pass: event.target.pass.value,
-        avatar: event.target.foto.value
-    }
-
-    const response = await fetch(
-        'http://localhost:3300/user',
-        {
-            cache: 'no-store',
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newUser) //tranforma um objeto em uma string json
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const newUser = {
+            name: event.target.name.value,
+            email: event.target.email.value,
+            pass: event.target.pass.value,
+            avatar: event.target.avatar.value
         }
-    )
-    if(response.ok){
-        const result = await response.json()
-        console.log(result)
-        if(result?.sucess){
-            setUsers([...users, result.user])
+        const response = await fetch(
+            'http://localhost:3300/user',
+            {
+                cache: 'no-store',
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newUser) //tranforma um objeto em uma string json
+            }
+        )
+        if (response.ok) {
+            const result = await response.json()
+            console.log(result)
+            if (result?.sucess) {
+                setUsers([...users, result.user])
+                setModalSignInIsOpen(false)
+            }
         }
     }
-}
 
     return (
         <>
             <h1>Membros</h1>
-            <button onClick={() => {setmodalCadastrarIsOpen(true)}}>Cadastrar</button>
+            <button onClick={() => setModalSignInIsOpen(true)}>Cadastrar</button>
             {
                 users.map((user) => { //map - usado para percorrer array. Não dá pra usar map em objeto.
                     return (
-                        <CardUser key={user.id} user={user} /> //forma do react identificar os cards. Diferenciar um card do outro
+                        <CardUser key={user.id} user={user} users={users} setUsers={setUsers} /> //forma do react identificar os cards. Diferenciar um card do outro
                     )
                 })
             }
-            <Modal isOpen={modalCadastrar} changeOpen={setmodalCadastrarIsOpen}>
+            <Modal isOpen={modalSignInIsOpen} changeOpen={setModalSignInIsOpen}>
                 <h1>Cadastros</h1>
                 <form onSubmit={handleSubmit}>
-                    Nome:<input type="text" name="name"/><br/>
-                    Email: <input type="text" name="email"/><br/>
-                    Pass: <input type="text" name="pass"/><br/>
-                    Foto: <input type="text" name="foto"/><br/>
-                    <input type="submit" value="Cadastrar"/>
+                    Nome:<input type="text" name="name" /><br />
+                    Email: <input type="text" name="email" /><br />
+                    Pass: <input type="text" name="pass" /><br />
+                    Foto: <input type="text" name="avatar" /><br />
+                    <input type="submit" value="Cadastrar" />
                 </form>
             </Modal>
 
